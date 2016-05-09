6256fe2b3222e562ffeb68474706290eb71cd6d6
y8932809 <529030008@qq.com>
2016-05-09 09:42:58 +0800

    第一次提交

---
+++ public/javascripts/chat_ui.js
@@ -0,0 +1,74 @@
+function  divEscapedContentElement(message){
+    return $('<div></div>').text(message);
+}
+
+function  divSystemContentElement(message){
+    return $('<div></div>').html('<li>'+message+'</li>');
+}
+//处理原始的用户输入
+function processUserInput(chatApp,socket){
+    var message=$('#send-message').val();
+    var systemMessage;
+    if(message.charAt(0)=='/'){
+        systemMessage=chatApp.processCommand(message);
+        if(systemMessage){
+            $('#messages').append(divSystemContentElement(message));
+        }
+    }
+    else{
+        chatApp.sendMeeage($('#room').text(),message);
+        $('#messages').append(divEscapedContentElement(message));
+        $('#messages').scrollTop($('#messages').prop('scrollHeigh'));
+
+    }
+    $("#send-message").val("");
+}
+
+//客户端程序初始化逻辑
+var socket=io.connect();
+$(document).ready(function(){
+    var chatApp=new Chat(socket);
+    socket.on('nameResult', function (result) {
+        var message;
+        if(result.success){
+            message='you are known as '+result.name+".";
+        }
+        else {
+            message = result.message;
+        }
+        $('#messages').append(divSystemContentElement(message));
+    });
+    socket.on('joinResult', function (result) {
+        $('#room').text(result.room);
+        $('#messages').append(divSystemContentElement('Room changed.'));
+    });
+
+    socket.on('message', function (message) {
+        var newElement=$('<div></div>').text(message.text);
+        $('#messages').append(newElement);
+    })
+
+    socket.on('rooms', function (rooms) {
+        $('#room-list').empty();
+        for(var room in rooms){
+            room=room.substring(1,room.length);
+            if(room!='') {
+                $('#room-list').append(divEscapedContentElement(room));
+            }
+        }
+        $('#room-list div').click(function () {
+            chatApp.processCommand('/join '+$(this).text());
+            $('#send-message').focus();
+        });
+    });
+
+    setInterval(function(){
+        socket.emit('rooms');
+    },1000);
+
+    $('#send-message').focus();
+    $('#send-form').submit(function () {
+        processUserInput(chatApp,socket);
+        return false;
+    });
+});
